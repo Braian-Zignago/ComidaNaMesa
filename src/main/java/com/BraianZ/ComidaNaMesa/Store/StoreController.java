c885c1a791f847751c4ec5a164346e4caf1809b0
Braian Zignago <braianzig77@gmail.com>
2025-09-01 21:48:55 -0300

    feat: create endpoint for getAll, getId and deleteById

--- src/main/java/com/BraianZ/ComidaNaMesa/Store/StoreController.java
+++ src/main/java/com/BraianZ/ComidaNaMesa/Store/StoreController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/store")
 @RequiredArgsConstructor
@@ -25,5 +27,33 @@ public class StoreController {
             return ResponseEntity.ok(storeResponseDTO);
         }
 
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getAll() {
+        List<StoreResponseDTO> storeResponseDTOList = storeService.getAll();
+        if (storeResponseDTOList == null) {
+            return ResponseEntity.internalServerError().body("Error getting stores");
+        }
+
+        return ResponseEntity.ok(storeResponseDTOList);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        StoreResponseDTO storeResponseDTO = storeService.getById(id);
+        if (storeResponseDTO == null) {
+            return ResponseEntity.status(404).body("Store with id: " + id + " not found");
+        }
+        return ResponseEntity.status(302).body(storeResponseDTO);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id) {
+        StoreResponseDTO storeResponseDTO = storeService.deleteById(id);
+        if (storeResponseDTO == null) {
+            return ResponseEntity.status(404).body("It was not possible to eliminate store with id: " + id + " not found");
+        }
+        return ResponseEntity.ok("Store: " + storeResponseDTO.name() + " with id: " + id + " was eliminated");
     }
 }
--- src/main/java/com/BraianZ/ComidaNaMesa/Store/StoreService.java
+++ src/main/java/com/BraianZ/ComidaNaMesa/Store/StoreService.java
@@ -1,7 +1,11 @@
 package com.BraianZ.ComidaNaMesa.Store;
 
+import com.BraianZ.ComidaNaMesa.Customer.CustomerModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +22,25 @@ public class StoreService {
         } catch (Exception e) {
             throw new RuntimeException("Error saving store: " + e.getMessage());
         }
+        return storeResponseDTO;
+    }
+
+    public List<StoreResponseDTO> getAll(){
+        List<StoreModel> storeModel = storeRepository.findAll();
+        return storeMapper.forStoreResponseDTOList(storeModel);
+    }
+
+    public StoreResponseDTO getById(Long id){
+        Optional<StoreModel> storeModel = storeRepository.findById(id);
+        return storeModel.map(storeMapper::forStoreResponseDTO).orElse(null);
+    }
+
+    public StoreResponseDTO deleteById(Long id){
+        StoreResponseDTO storeResponseDTO = getById(id);
+        if(storeResponseDTO == null){
+            return null;
+        }
+        storeRepository.deleteById(id);
         return storeResponseDTO;
     }
 }
